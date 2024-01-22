a5367219b24624aaa106acdc2638b7e1f9e8e979
malte <malte.lindkvist@elev.ga.ntig.se>
2024-01-22 11:58:13 +0100

    klar med 2 bilder och historian

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 const app = express()
 
